fec033eaced124caa34071f03b3466d32f541abf
sbhusal123 <suryabhusal11@gmail.com>
2020-09-05 13:06:57 +0545

    React react learning project setup.

---
+++ react-redux-demo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CakeContainer from './Components/CakeContainer'
+
+
+function App() {
+  return (
+    <div className="App">
+      <CakeContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-redux-demo/src/Components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CakeContainer() {
+    return (
+        <div>
+            <h2>Number of cakes</h2>
+            <button>Buy Cakes</button>
+        </div>
+    )
+}
+
+export default CakeContainer
