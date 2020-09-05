f031f941b602c61e4d5eb1467d828f6259d6ae8e
sbhusal123 <suryabhusal11@gmail.com>
2020-09-05 13:57:07 +0545

    Added useDispatch hooks: dispatching action without connect.

--- react-redux-demo/src/Components/HooksCakeContainer.js
+++ react-redux-demo/src/Components/HooksCakeContainer.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
-// Without Using Connect
+import { buyCake } from '../redux'
+
+// Without Using Connect, Using Hooks to dispatch and pass the props
+
+// Usage warning: https://react-redux.js.org/api/hooks#useselector
 
 function HooksCakeContainer() {
 
     const numOfCakes = useSelector(state => state.numOfCakes)
-
+    const dispatch = useDispatch()
     return (
         <div>
             <h2>Num of Cakes - {numOfCakes} </h2>
-            <button>Buy Cake</button>
+            <button onClick={ () =>dispatch(buyCake())}>Buy Cake</button>
         </div>
     )
 }
